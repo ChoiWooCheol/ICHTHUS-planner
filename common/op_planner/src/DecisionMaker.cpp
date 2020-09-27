9eb03f9b1355744b30dc8af100acd187cce6ce76
ChoiWooCheol <smartchoi09@naver.com>
2020-09-27 18:51:06 +0900

    Update DecisionMaker.cpp

--- common/op_planner/src/DecisionMaker.cpp
+++ common/op_planner/src/DecisionMaker.cpp
@@ -511,7 +511,7 @@ void DecisionMaker::SetMaxVelocityParam(double max_speed)
 			double distance_to_stop = followDistance -  critical_long_front_distance - additionalBrakingDistance;
 			double sudden_stop_distance = -pow((CurrStatus.speed - beh.followVelocity), 2)/m_CarInfo.max_deceleration;
 			/* sum(weights) must be 1 */
-			// double f_distanceRateVelocity = 0.5 * sqrt(0.001 * pow(followDistance, 3)) + (0.000015 * pow(followDistance, 2));
+			// double f_distanceRateVelocity = sqrt(0.001 * pow(followDistance, 3)) - (0.001 * pow(followDistance, 2));
 			// double f_distanceRateVelocity = -1 * sqrt(0.000001 * pow(followDistance, 3)) + (0.005 * pow(followDistance, 2));
 			double f_distanceRateVelocity = 0.5 * followDistance * 0.18; // weight1 * (distance / time)
 			double f_velocityRateVelocity = 0.3 * beh.followVelocity; 	 // weight2 * followVelocity
